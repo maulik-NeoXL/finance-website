3b6f9c3a64b5fa0a5aa1b171b2dcc33dd1fcbcd0
maulik-NeoXL <maulik@neoxl.com>
2023-05-31 00:03:40 +0530

    Stats

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 
-export default function Button()
+export default function Button({styles})
 {
   return (
-    <div>
-        Button
-    </div>
+    <button type={'button'} className={`py-4 px-6 bg-blue-gradient text-[18px] text-primary outline-none ${styles} font-medium rounded-xl`}>
+      Get Started
+    </button>
   );
 }
